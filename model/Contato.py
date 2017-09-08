e3c8e9e8260c213601810a24eb1cfe1ca056801d
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-09-08 20:15:05 -0300

    Update Contato.py

--- model/Contato.py
+++ model/Contato.py
@@ -1,29 +1,35 @@
-class Contato():
- 
- 	def __init__(self, criacao, nome,nascimento,email,numero,ddd,codigoPais):
-   
- 		self.criacao = criacao
-   
- 		self.nome = nome
-    
-		self.nascimento = nascimento
-  
-  		self.email = email
-   
- 		self.numero = numero
-    
-		self.ddd = ddd
-    
-		self.codigoPais = codigoPais
-    
-  
 
- 	def __str__(self):
-   
- 		return "Contato: %s nascimento: %s email: %s numero: %s ddd: %s codigo Pais: %s"%(self.nome,self.nascimento,self.email,self.numero,self.ddd,self.codigoPais)
-    
-    
- 
- 	def listarTelefones(self):
-   
- 		return []
+'''
+Criação da classe do Objeto Contato e
+os seus atributos de um Contato;
+'''
+
+    # Importação das classes necessarias para a execução do Contato;
+from model.Pessoa import Pessoa
+from model.Telefone import Telefone
+import datetime
+import json
+
+    # Criação de uma lista de Contatos;
+relacaoContato = []
+
+    # Classe do Objeto contato da agenda telefonica;
+class Contato(Telefone):
+
+    # (Precisa comentar)
+    def __init__(self, criacao, pessoa):
+        self.criacao = datetime.date.today() # Mostra a data do dia da execução do programa
+        self.pessoa = Pessoa
+        self.telCont = Telefone()
+        self.contato = (str(self.criacao)+" "+ str(self.pessoa)+" "+ str(self.telCont))
+
+        # Essa função (append), vai adicionar cada telefone em uma lista;
+        relacaoContato.append(self.contato)
+
+    # (Precisa comentar)
+    def listarTelefones(self):
+        return self.telefones
+
+    # Método mágico __str__ do Contato;
+    def __str__(self):
+        return("Data da Criação: "+ str(self.criacao)+"\nContato: \n"+str(self.pessoa)+"\nNúmero: "+str(self.telCont))
