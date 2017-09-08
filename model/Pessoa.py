7a605dbc8f17f719f24fd444141a0f7f6c42fa9d
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-09-08 20:16:18 -0300

    Update Pessoa.py

--- model/Pessoa.py
+++ model/Pessoa.py
@@ -1,13 +1,17 @@
+import json
+
+    # Importação da biblioteca que formata a data de nscimento do usuario;
+import datetime
+
+    # Classe do Objeto Pessoa da agenda telefonica;
 class Pessoa():
-  
-	def __init__(self, nome, nascimento, email):
-    
-		self.nome = nome
-    self.nascimento = nascimento
-    
-		self.email = email
-    
-  
+
+    # Função que vai receber os paramêtros informados no exercicío;
+    def __init__(self,nome,nascimento,email):
+        self.nome = str(input("Infome o seu nome:"))
+        self.nascimento = datetime.date()
+        self.email = str(input("Infome o seu email:"))
+
+    # Função __str__ (método mágico) do Objeto Pessoa;
 	def __str__(self):
-    
 		return "Proprietario: %s Nascimento: %s email: %s "%(self.nome,self.nascimento,self.email)
