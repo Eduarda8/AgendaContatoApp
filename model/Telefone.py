1ed561aa5c4940706cb5c9502785a2bf4abc3dc5
Eduarda Alves <Eduarda8@users.noreply.github.com>
2017-09-19 17:49:10 -0300

    Update Telefone.py

--- model/Telefone.py
+++ model/Telefone.py
@@ -3,21 +3,16 @@ Criação da Classe do objeto Telefone e adicionando
 os seus atributos do telefone para cada contato;
 '''
 
-# Criação da lista de telefones;
-relacaoTelefone = []
-
 class Telefone():
 
     # Função que vai receber os paramêtros informados no exercicío;
-    def __init__(self,numero,ddd, codigoPais):
+    def __init__(self, numero, ddd, codigoPais):
         self.numero = numero
         self.ddd = ddd
         self.codigoPais = codigoPais
-        self.telefone = ("(" + str(self.numero) + " " + str(self.ddd) + ") " + str(self.codigoPais))
 
-        # Será adicionado cada telefone em uma lista de telefone especifica;
-        relacaoTelefone.append(self.telenone)
+     # self.telefone = ("(" + str(self.numero) + " " + str(self.ddd) + ") " + str(self.codigoPais))
 
     # Método mágico __str__ do telefone;
     def __str__(self):
-        return self.telefone
+        return ("(" + str(self.numero) + " " + str(self.ddd) + ") " + str(self.codigoPais))
